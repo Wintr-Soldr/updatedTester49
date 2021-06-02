e6b597f61859f21dccd1624dec18e107492df570
diya18114021 <45495906+diya18114021@users.noreply.github.com>
2021-06-02 21:36:27 +0530

    Update MyAdapter.kt

--- app/src/main/java/com/example/tester49/MyAdapter.kt
+++ app/src/main/java/com/example/tester49/MyAdapter.kt
@@ -35,10 +35,8 @@ class MyAdapter(private val mContext: Context, resource: Int, objects: List<Cate
             val layoutInflator = LayoutInflater.from(mContext)
             convertView = layoutInflator.inflate(R.layout.dorpdown_checkbox_item, null)
             holder = ViewHolder()
-            holder.mTextView = convertView
-                .findViewById<View>(R.id.text) as TextView
-            holder.mCheckBox = convertView
-                .findViewById<View>(R.id.checkbox) as CheckBox
+            holder.mTextView = convertView.findViewById<View>(R.id.text) as TextView
+            holder.mCheckBox = convertView.findViewById<View>(R.id.checkbox) as CheckBox
             convertView.tag = holder
         } else {
             holder = convertView.tag as ViewHolder
@@ -48,29 +46,26 @@ class MyAdapter(private val mContext: Context, resource: Int, objects: List<Cate
         isFromView = true
         holder.mCheckBox!!.isChecked = listState[position].isSelected()
         isFromView = false
-        if (position == 0) {
-            holder.mCheckBox!!.visibility = View.INVISIBLE
-        } else {
-            holder.mCheckBox!!.visibility = View.VISIBLE
-        }
+        holder.mCheckBox!!.visibility = View.VISIBLE
         holder.mCheckBox!!.tag = position
         holder.mCheckBox!!.setOnCheckedChangeListener { buttonView, isChecked ->
             val getPosition = buttonView.tag as Int
             if (!isFromView) {
                 listState[position].setSelected(isChecked)
+                if(listState[0].isSelected()){
+                    listState[position].setSelected(true)
+                }
                 Log.i("checkpos", "" + listState[position].getTitle())
                 Log.i("valueif", "check$isFromView")
                 Toast.makeText(context, "" + listState[position].isSelected(), Toast.LENGTH_LONG)
                     .show()
                 if (listState[position].isSelected()) {
-                    Log.i("showStatus", "You are beast")
                     val sharedPreferences =
                         mContext.getSharedPreferences("pref", Context.MODE_PRIVATE)
                     val editor = sharedPreferences.edit()
                     editor.putString(listState[position].getTitle(), listState[position].getTitle())
                     editor.apply()
                 } else {
-                    Log.i("showStatus", "You are Nut")
                     val sharedPreferences =
                         mContext.getSharedPreferences("pref", Context.MODE_PRIVATE)
                     val editor = sharedPreferences.edit()
@@ -93,6 +88,3 @@ class MyAdapter(private val mContext: Context, resource: Int, objects: List<Cate
     }
 }
 
-private fun TextView?.setText(title: Any) {
-
-}
