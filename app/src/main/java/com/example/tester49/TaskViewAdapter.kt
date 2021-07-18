b24bdbafa37a4267ca978a5f6afe199994d46950
Wintr-Soldr <84572134+Wintr-Soldr@users.noreply.github.com>
2021-07-18 05:55:57 +0530

    Update TaskViewAdapter.kt

--- app/src/main/java/com/example/tester49/TaskViewAdapter.kt
+++ app/src/main/java/com/example/tester49/TaskViewAdapter.kt
@@ -7,18 +7,18 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 //TTV
 class TaskViewAdapter:RecyclerView.Adapter<TaskViewAdapter.ViewHolder>() {
+//variables used 
+
+    private val titles= arrayOf("Task Name 1","Task Name 2","Task Name 3")//names of tasks
+    private val numpeople= arrayOf("5","5","6")//number of people in task
+    private val startdates= arrayOf("21/12/2111","28/11/2590","11/11/1111")//starting date of task
+    private val descc= arrayOf("This is desc\n..........\n..........","This is desc\n..........\n..........","This is desc\n..........\n..........")//desc of task
+
+    private val enddate= arrayOf("21/12/2121","28/11/2600","11/11/1110")//end date of task
 
 
-    private val titles= arrayOf("Task Name 1","Task Name 2","Task Name 3")
-    private val numpeople= arrayOf("5","5","6")
-    private val startdates= arrayOf("21/12/2111","28/11/2590","11/11/1111")
-    private val descc= arrayOf("This is desc\n..........\n..........","This is desc\n..........\n..........","This is desc\n..........\n..........")
 
-    private val enddate= arrayOf("21/12/2121","28/11/2600","11/11/1110")
-
-
-
-
+//creating view and assigning values
     inner class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
 
 
@@ -52,7 +52,7 @@ class TaskViewAdapter:RecyclerView.Adapter<TaskViewAdapter.ViewHolder>() {
         viewType: Int
     ): ViewHolder {
 
-
+//inflating view
 
         val v= LayoutInflater.from(parent.context)
             .inflate(R.layout.task_view_model,parent,false)
@@ -62,6 +62,7 @@ class TaskViewAdapter:RecyclerView.Adapter<TaskViewAdapter.ViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        //assigning values to view
         holder.Tasktitle.text=titles[position]
         holder.Tasknumpeople.text=numpeople[position]
         holder.Taskstartdate.text=startdates[position]
